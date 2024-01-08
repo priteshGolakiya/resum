0a180b1b05c6133d83e11e33cf66b9ca8facbe52
Pritesh Golakiya <priteshgolakiya1@gmail.com>
2024-01-08 21:58:51 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
+import SplashScreen from "./pages/SplashScreen"; // Import SplashScreen
 import Services from "./pages/Services";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -9,18 +10,43 @@ import Portfolio from "./pages/Portfolio";
 import Resume from "./pages/Resume";
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    // Hide SplashScreen after 5000 milliseconds (5 seconds)
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            {/* <Route path="/services" element={<Services />} /> */}
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-          </Route>
+          {showSplash && (
+            <Route
+              path="/"
+              element={
+                <SplashScreen onSplashComplete={() => setShowSplash(false)} />
+              }
+            />
+          )}
+
+          {!showSplash && (
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/resume" element={<Resume />} />
+              {/* <Route path="/services" element={<Services />} /> */}
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/contact" element={<Contact />} />
+            </Route>
+          )}
+
+          {/* Redirect to home if the route is not matched */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/SplashScreen.jsx
@@ -0,0 +1,81 @@
+// SplashScreen.js
+import React, { useEffect, useState } from "react";
+import styles from "../styles/home/SplashScreen.module.css";
+import Home from "./Home";
+
+function SplashScreen({ onSplashComplete }) {
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    const loadingBar = document.querySelector(`.${styles.loadingbar}`);
+    const loadingBox = document.querySelector(`.${styles.loadingBox}`);
+    const splashScreen = document.querySelector(`.${styles.splashScreen}`);
+    const loadingCircle = document.querySelector(`.${styles.loadingCircle}`);
+    const body = document.querySelector("body");
+
+    const startAnimations = () => {
+      setTimeout(() => {
+        if (loadingBar) loadingBar.style.left = "0";
+      }, 1500);
+
+      setTimeout(() => {
+        if (loadingBox) loadingBox.style.opacity = "1";
+      }, 500);
+
+      setTimeout(() => {
+        if (splashScreen) splashScreen.style.top = "-100%";
+      }, 4500);
+
+      setTimeout(() => {
+        if (loadingCircle) loadingCircle.style.opacity = "0";
+      }, 4500);
+
+      setTimeout(() => {
+        if (body) {
+          body.classList.add("visibleSplash");
+        }
+        setAnimationComplete(true);
+        onSplashComplete();
+      }, 3000);
+    };
+
+    startAnimations();
+
+    return () => {
+      // Cleanup functions if needed
+    };
+  }, [onSplashComplete]);
+
+  return (
+    <>
+      {animationComplete ? (
+        <Home />
+      ) : (
+        <div className={styles.splashBody}>
+          <div className={styles.splashScreen}>
+            <div className={styles.loadingContainer}>
+              <div className={styles.loadingBox}>
+                <img
+                  src="https://vignette.wikia.nocookie.net/borderlands/images/4/42/Vault_logo.png/revision/latest?cb=20100114181536"
+                  alt="Splash Logo"
+                  className={styles.splashLogo}
+                />
+                <div className={styles.loadingBarContainer}>
+                  <div className={styles.loadingbar}></div>
+                </div>
+
+                <div className={styles.loadingCircle}>
+                  <div className={styles.circleOuter}></div>
+                  <div className={styles.circleLoader}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.splashBehind}></div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default SplashScreen;
